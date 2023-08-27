6f4e0665e5c409eaf0800f0e151cce57a1591460
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-08-27 17:03:48 +0200

    fexid css

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,15 @@
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Modal } from 'components/Modal/modal.styled';
-import { List } from './ImageGallery.styled';
+
+import { ListGallery } from './ImageGallery.styled';
 
 export const ImageGallery = ({ image }) => {
   return (
     <>
-      <List>
+      <ListGallery>
         {image.map(im => (
           <ImageGalleryItem key={im.id} image={im} />
         ))}
-      </List>
-      <Modal image={image.largeImageURL} />
+      </ListGallery>
     </>
   );
 };
