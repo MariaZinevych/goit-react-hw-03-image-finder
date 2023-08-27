622ef8788037f913565524003a4598f049fa8ba7
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-08-27 17:27:51 +0200

    -*

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,22 +1,4 @@
-import { Component } from 'react';
-import { Overlay, Modal } from './modal.styled';
 
-export class Modal extends Component {
-  state = { isModalOpen: false };
-  openModal = () => this.setState({ isModalOpen: true });
 
-  closeModal = () => this.setState({ isModalOpen: false });
 
-  render() {
-    return (
-      <>
-        <Overlay>
-          <Modal>
-            isOpen={this.state.isModalOpen}
-            <button onClick={this.closeModal}>close</button>
-          </Modal>
-        </Overlay>
-      </>
-    );
-  }
-}
+
